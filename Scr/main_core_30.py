3d77da1e3926cc4187dbc3ffa0492f7525712546
hiep-vuotqua <hiepcodonchuayeu@gmail.com>
2026-09-22 01:57:54 +0700

    Update main_core_30.py

--- Scr/main_core_30.py
+++ Scr/main_core_30.py
@@ -80,7 +80,7 @@ def send_telegram(msg):
 
 def process_ticker(ticker, state, tz, session, now):
     try:
-        df = get_intraday_data(ticker, days=50a0)
+        df = get_intraday_data(ticker, days=500)
     except Exception as e:
         print(f"  [{ticker}] ERROR data: {e}")
         return state
